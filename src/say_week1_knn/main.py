260fa0916c7f8e751b683df268f8bc2c1309cbfb
NishNovae <raecrowned@gmail.com>
2024-09-09 11:28:14 +0900

    model made but testing error

--- src/say_week1_knn/main.py
+++ src/say_week1_knn/main.py
@@ -3,19 +3,31 @@
 import numpy as np
 import os
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 import pickle
+import random
 
 data_list = []
-kn = KNeighborsClassifier()
-#kn.fit([30, 600], [1])
+kn = KNeighborsClassifier(n_neighbors=5)
+NOT_PREPARED = True
+x_data, y_data = [], []
+
+def create_dummy():
+    global x_data, y_data
+    x_data, y_data = [], []
+    for i in range (10):
+        x_data.append( [round(random.uniform(10, 41), 2), round(random.uniform(10, 1000), 2) ])
+        y_data.append( random.choice( ["Bream", "Smelt"] ) )
 
 def yes_or_no(prompt):
     while True:
-        yn = input(prompt + " (y/n)")
+        yn = input(prompt + " [y/n] ")
         if yn == "y" or yn == "Y":
-            return True
+            return 1
         elif yn == "n" or yn == "N":
-            return False
+            return 0
+        elif yn == "exit":
+            return -1
         else:
             print("Wrong format. Please input either 'y' or 'n'. Retrying...")
             continue
@@ -31,75 +43,125 @@ def get_model_path(model="model.pkl"):
     return model_path
 
 def parse_data(data_path):
-    global input_val, output_val 
+    global x_data, y_data
+
     with open(data_path, "r") as data:
+        next(data)      # skips line "Length,Width,Label"
         for line in data:
-            data_list.append(line.strip().split(","))    
+            length, width, answer = line.strip().split(",")
+            x_data.append([float(length), float(width)])
+            y_data.append(answer)
+
     print(f"[INFO] data collected from: {data_path}")
     return
 
-def bulk_train():
-    for length, width, answer in data_list:
-        prediction = predict(length, width)
-        if prediction == answer:
-            update()
-            print("Correct!")
-        else:
-            update_wrong()
-            print("Wrong!")
+def bulk_train(x_list=[], y_list=[]):
+    global NOT_PREPARED, x_data, y_data
+    NOT_PREPARED = False
+
+    for i in range (len(x_list)):
+        x_data.append(x_list[i])
+        y_data.append(y_list[i])
+        print(x_data[-2:])
+        print(y_data[-2:])
+
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=42)
+    kn.fit(x_train, y_train)
+
+    # debug
+    print("data len: ", len(x_train))
+    bulk_test(x_test, y_test)
+
+def bulk_test(x_test, y_test):
+    prediction = kn.predict(x_test)
+    print("y_test length: ", len(y_test))
+    tmp = 0
+    for i in range(len(y_test)):
+        if prediction[i] == y_test[i]:
+            tmp += 1
+    
+    acc = tmp/len(y_test) * 100
+    print(f"RESULT: {acc} % accuracy\n")
+
 
 def prompt():
+    global NOT_PREPARED
+    if NOT_PREPARED:
+        print("[INFO] Initializing dummy data of size 10...\n")
+        create_dummy()
+        bulk_train()
+
+    print("[INFO] Proceeding to manual prompt.\n")
+    print("[INFO] User prompt activated! Please input the length and width.")
     global input_val, output_val
-    print("User prompt activated! Please input the length and width.")
     while True:
         try:
-            length = input("length: ")
+            print()
+            line = input("length, width [whitespace-separated]: ")
+            length, width = line.strip().split()
             length = float(length)
-
-            width = input("weight: ")
             width = float(width)
+
         except ValueError:
             print("[ERROR] length/width should be convertable to floating point values. Please try again.\n")
             continue
+
         else:
-            prediction = predict(length, width)
+            val, prediction = predict(length, width)
             print(f"The prediction for given length: {length}, width: {width} is: {prediction}")
-
             correct = yes_or_no("Was this prediction correct?")
-            if correct:
-                update_correct() # updates model
+
+            if correct == 1:
+                update(length, width, val) # updates model
+            elif correct == 0:
+                update(length, width, 1-val)
             else:
-                update_wrong()
-            
-            print("[INFO] Model updated!")
-            finish = yes_or_no("Finish training?")
-            if finish:
+                print("Exiting...")
                 return
-            else:
-                continue
+            
+            # print("[INFO] Model updated!")
 
-def predict(length, width, initial_pred = "Bream"):
+def predict(length, width):
     # training data is data_list
-    pred = initial_pred
-#    if kn.predict([[width, length]])[0] == 1:
-#        return "Smelt"
-#    else:
-#        return "Bream"
-#    return pred
-    return pred
 
-def update_correct():
-    pass
+    if kn.predict([[length, width]])[0] == 1: # 1 == Smelt
+        return 1, "Smelt"
+    else:
+        return 0, "Bream"
 
-def update_wrong():
-    pass
+def update(length, width, val):
+
+    print(f"length:{length}, width:{width}, val:{val}")
+
+    if val == 1:
+        answer = "Smelt"
+    elif val == 0:
+        answer = "Bream"
+    else:
+        return
+    bulk_train([[length, width]], [answer])
+
+def save_model():
+    model_path = get_model_path()
+    with open(model_path, "wb") as model:
+        pickle.dump(kn, model)
 
 # debug
-manual = yes_or_no("Welcome! Do you wish to train the model manually?")
-if manual:
-    print("Proceeding to manual training.\n")
-    prompt()
-else:
-    print("Proceeding to training from prefab data.\n")
+print("[INFO] Beginning fish prediction program! type 'exit' to finish whenever a (y/n) prompt comes up.\n")
+bulk = yes_or_no("Welcome! Do you wish to bulk-train the model before entering prompt?")
+
+if bulk == 1:
+    print("[INFO] Proceeding to bulk-train from prefab data...\n")
     parse_data(grab_data_path())
+#    print(data_list)
     bulk_train()
+    print("[INFO] Bulk-train complete!\n")
+    prompt()
+
+elif bulk == 0:
+    prompt()
+
+else:
+    print("[INFO] Finishing program...")
+save_model()
+
